654b9c2330ef4e7235f84252c6151975058a643b
Roba <robamagedaskar321@gmail.com>
2024-04-21 14:41:33 +0200

    discovery,api gateway,user table

---
+++ security/security/src/main/java/com/roba/security/JwtService.java
@@ -0,0 +1,12 @@
+package com.roba.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+
+    public String extractEmail(String token) {
+        return null;
+    }
+}
---
+++ security/security/src/main/java/com/roba/security/User.java
@@ -0,0 +1,77 @@
+package com.roba.security;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.management.relation.Role;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Table(name="_user")
+public class User implements UserDetails {
+    @Id
+    @SequenceGenerator(
+            name="user_id_sequence",
+            sequenceName="user_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_id_seqeunce"
+    )
+    private Integer id;
+    private String fullName;
+    private String workEmail;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    private LocalDate joinDate;
+    //foreign key to organization table
+    private Integer organizationId;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.getRoleName()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return workEmail;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
